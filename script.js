966087c445b582fc7c745aebe6a38e1096042750
Rohit-290 <rohit290kumar@gmail.com>
2026-08-18 17:44:09 +0530

    Updated on 18th August 2026

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const box = document.querySelectorAll(".box");
 const btn = document.querySelector(".btn");
-
-
-
-
-
+const aud = document.querySelector(".aud");
+let counter = 0; 
 
 document.addEventListener("keydown",(e)=>{
-   
     let num = `b${random(8)}`;
     const gen = document.getElementById(`${num}`);
-    gen.innerText = e.key.toUpperCase();
+    let col = `rgb(${random(255)} ${random(255)} ${random(255)})` ;
+    if(gen.innerText = e.key.toUpperCase()){
+        const con = document.querySelector(".c2");
+        con.innerText = counter++ ;
+    };
+    gen.style.color = col ;
+    aud.play();
     setTimeout(() => {
-     gen.innerText = "";}, 3000);
+     gen.innerText = "";
+    }, 3000);
 });
+
 
 const cleandiv = () => {
      gen.innerText = "";
@@ -21,4 +25,4 @@ const cleandiv = () => {
 
  function random(number){
     return Math.floor(Math.random()*number)+1;
- }
+ }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
