09aad23c9c9f08aeefccdcb2af8875274fbd18f1
Rohit-290 <rohit290kumar@gmail.com>
2026-09-05 16:13:38 +0530

    Updated on 5th September 2026

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
 const btn = document.querySelector(".btn");
 const aud = document.querySelector(".aud");
 const aud1 = document.querySelector(".aud1");
-const anime = document.querySelector(".anime");
+const anime = document.getElementsByClassName(".anime");
 const panda = document.querySelector(".panda");
+const ball0 = document.querySelector(".ball0");
 const ball1 = document.querySelector(".ball1");
 const ball2 = document.querySelector(".ball2");
 const ball3 = document.querySelector(".ball3");
 const ball4 = document.querySelector(".ball4");
 const ball5 = document.querySelector(".ball5");
-const ball6 = document.querySelector(".ball6");
 
 /* setting values----------------------------------------------xxxxx-----------------------------------------------xxxxxxxxxxxxxxxxxxxx--------------------------------------------------------------------------*/
 
@@ -20,6 +20,7 @@ function random(number){
     return Math.floor(Math.random()*number)+1;
  }                      
  
+
 
 /* building functions----------------------------------------------xxxxx-----------------------------------------------xxxxxxxxxxxxxxxxxxxx--------------------------------------------------------------------------*/
 
@@ -57,26 +58,57 @@ const cleandiv = () => {
     gen.innerText = "" ;
 }
 
-// let ballonceleb = () => {
 
-//      ball1.style.visibility = "visible";
-//      ball2.style.visibility = "visible";
-//      ball3.style.visibility = "visible";
-//      ball4.style.visibility = "visible";
-//      ball5.style.visibility = "visible";
-//      ball6.style.visibility = "visible";
-    
-//      setTimeout(() => {
-//      anime.style.visibility = "hidden";
-//      ball1.style.visibility = "hidden";
-//      ball2.style.visibility = "hidden";
-//      ball3.style.visibility = "hidden";
-//      ball4.style.visibility = "hidden";
-//      ball5.style.visibility = "hidden";
-//      ball6.style.visibility = "hidden";
-    
-//     }, 9000);
-// }
+
+const animation0 = ball0.animate(
+    [ { transform: "translate(0px,0px)"},{transform: "translate(-50px,-50px)"},{transform: "translate(50px,-100px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-150px)"},
+        {transform: "translate(50px,-200px) skew(-6deg,-4deg)" },{transform: "translate(-50px,-250px)"},{transform: "translate(50px,-300px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-350px)"},
+        {transform: "translate(50px,-400px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-450px)"},{transform: "translate(50px,-500px)skew(-6deg,-4deg)"}]
+   ,{duration: 10000,
+    iteration: "Infinity",
+    }
+)
+
+const animation1 = ball1.animate(
+    [ { transform: "translate(0px,0px)"},{transform: "translate(-50px,-50px)"},{transform: "translate(50px,-100px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-150px)"},
+        {transform: "translate(50px,-200px) skew(-6deg,-4deg)" },{transform: "translate(-50px,-250px)"},{transform: "translate(50px,-300px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-350px)"},
+        {transform: "translate(50px,-400px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-450px)"},{transform: "translate(50px,-500px)skew(-6deg,-4deg)"}]
+   ,{duration: 10000,
+    iteration: "Infinity"}
+)
+
+const animation2 = ball2.animate(
+    [ { transform: "translate(0px,0px)"},{transform: "translate(-50px,-50px)"},{transform: "translate(50px,-100px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-150px)"},
+        {transform: "translate(50px,-200px) skew(-6deg,-4deg)" },{transform: "translate(-50px,-250px)"},{transform: "translate(50px,-300px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-350px)"},
+        {transform: "translate(50px,-400px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-450px)"},{transform: "translate(50px,-500px)skew(-6deg,-4deg)"}]
+   ,{duration: 10000,
+    iteration: "Infinity"}
+)
+
+const animation3 = ball3.animate(
+    [ { transform: "translate(0px,0px)"},{transform: "translate(-50px,-50px)"},{transform: "translate(50px,-100px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-150px)"},
+        {transform: "translate(50px,-200px) skew(-6deg,-4deg)" },{transform: "translate(-50px,-250px)"},{transform: "translate(50px,-300px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-350px)"},
+        {transform: "translate(50px,-400px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-450px)"},{transform: "translate(50px,-500px)skew(-6deg,-4deg)"}]
+   ,{duration: 10000,
+    iteration: "Infinity"}
+)
+
+const animation4 = ball4.animate(
+    [ { transform: "translate(0px,0px)"},{transform: "translate(-50px,-50px)"},{transform: "translate(50px,-100px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-150px)"},
+        {transform: "translate(50px,-200px) skew(-6deg,-4deg)" },{transform: "translate(-50px,-250px)"},{transform: "translate(50px,-300px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-350px)"},
+        {transform: "translate(50px,-400px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-450px)"},{transform: "translate(50px,-500px)skew(-6deg,-4deg)"}]
+   ,{duration: 10000,
+    iteration: "Infinity"}
+)
+
+const animation5 = ball5.animate(
+    [ { transform: "translate(0px,0px)"},{transform: "translate(-50px,-50px)"},{transform: "translate(50px,-100px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-150px)"},
+        {transform: "translate(50px,-200px) skew(-6deg,-4deg)" },{transform: "translate(-50px,-250px)"},{transform: "translate(50px,-300px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-350px)"},
+        {transform: "translate(50px,-400px)skew(-6deg,-4deg)"},{transform: "translate(-50px,-450px)"},{transform: "translate(50px,-500px)skew(-6deg,-4deg)"}]
+   ,{duration: 10000,
+    iterations: "Infinity"}
+)
+
 
 /* event handling----------------------------------------------xxxxx-----------------------------------------------xxxxxxxxxxxxxxxxxxxx--------------------------------------------------------------------------*/
 
@@ -85,10 +117,27 @@ document.addEventListener("keydown",(e)=>{
     let col = `rgb(${random(255)} ${random(255)} ${random(255)})` ;
     if(gen.innerText = e.key.toUpperCase()){
         playBtn();  
+       
+        if(counter == 50){
+        ball0.style.visibility = "visible";
+        ball1.style.visibility = "visible";
+        ball2.style.visibility = "visible";
+        ball3.style.visibility = "visible";
+        ball4.style.visibility = "visible";
+        ball5.style.visibility = "visible";
+        animation0.play();
+        animation1.play();
+        animation2.play();
+        animation3.play();
+        animation4.play();
+        animation5.play();
+        }
+
         const con = document.querySelector(".c2");
         con.innerText = counter ++ ;
         gen.style.transform = `translate(${random(1200)}px , ${random(400)}px)`;
     }
+    
     gen.style.color = col ;
     setTimeout(() => {
         gen.innerText = "" 
@@ -96,6 +145,14 @@ document.addEventListener("keydown",(e)=>{
         if(counter == 2){
         playBgm();
     }
+      setTimeout(() => {
+        ball0.style.visibility = "hidden";
+        ball1.style.visibility = "hidden";
+        ball2.style.visibility = "hidden";
+        ball3.style.visibility = "hidden";
+        ball4.style.visibility = "hidden";
+        ball5.style.visibility = "hidden";
+        } , 8000);
         
 })
 
--- xcxc.js
+++ xcxc.js
@@ -1,165 +1,244 @@
-let questions = [
-   { 
-    category: "History" ,
-    question: "Archduke Franz Ferdinand was heir to which empire ?" ,
-    choices: ["Prussian","Ottoman","Austro-Hungarian"] ,
-    answer: "Austro-Hungarian" ,
-  },
-   {
-    category: "Science" ,
-    question: "What is known as powerhouse of a cell?" ,
-    choices: ["Mitochondria","Ribosomes","Vacoules"],
-    answer: "Mitochondria" ,
-  },
-   {
-    category: "Geography" ,
-    question: "Which mountain range borders Europe and Asia?" ,
-    choices: ["The Himalayan","The Alps","Ural"] ,
-    answer: "Ural" ,
-  },
-   {
-    category: "Astronomy" ,
-    question: "How many moons does Mars has?" ,
-    choices: ["1","2","3"] ,
-    answer: "2" ,
-  },
-   {
-    category: "Computer Science" ,
-    question: "Who is known as the father of World Wide Web?" ,
-    choices: ["Tim Berners Lee","Charles Babbage","Steve Jobs"] ,
-    answer: "Tim Berners Lee" ,
-  },
-];
+// let questions = [
+//    { 
+//     category: "History" ,
+//     question: "Archduke Franz Ferdinand was heir to which empire ?" ,
+//     choices: ["Prussian","Ottoman","Austro-Hungarian"] ,
+//     answer: "Austro-Hungarian" ,
+//   },
+//    {
+//     category: "Science" ,
+//     question: "What is known as powerhouse of a cell?" ,
+//     choices: ["Mitochondria","Ribosomes","Vacoules"],
+//     answer: "Mitochondria" ,
+//   },
+//    {
+//     category: "Geography" ,
+//     question: "Which mountain range borders Europe and Asia?" ,
+//     choices: ["The Himalayan","The Alps","Ural"] ,
+//     answer: "Ural" ,
+//   },
+//    {
+//     category: "Astronomy" ,
+//     question: "How many moons does Mars has?" ,
+//     choices: ["1","2","3"] ,
+//     answer: "2" ,
+//   },
+//    {
+//     category: "Computer Science" ,
+//     question: "Who is known as the father of World Wide Web?" ,
+//     choices: ["Tim Berners Lee","Charles Babbage","Steve Jobs"] ,
+//     answer: "Tim Berners Lee" ,
+//   },
+// ];
 
-function getRandomQuestion (questions){
-   return questions[`${(random(5))}`];
-} 
+// function getRandomQuestion (questions){
+//    return questions[`${(random(5))}`];
+// } 
 
-let z = questions[`${(random(5))}`].choices  ;
-let  c = function getRandomComputerChoice (z){
-   return z[`${(random(2))}`];
-} 
-
-
+// let z = questions[`${(random(5))}`].choices  ;
+// let  c = function getRandomComputerChoice (z){
+//    return z[`${(random(2))}`];
+// } 
 
 
-let a = questions[`${(random(5))}`];
-// let c = questions[`${(random(5))}`].choices[`${(random(3))}`];
-function getResults (a, c){
-   let e = a.answer;
-  if(e === c){
-    return "The computer's choice is correct!" ;
-  }else{
-    return "The computer's choice is wrong." + "The correct answer is:" + e ;
+
+
+// let a = questions[`${(random(5))}`];
+// // let c = questions[`${(random(5))}`].choices[`${(random(3))}`];
+// function getResults (a, c){
+//    let e = a.answer;
+//   if(e === c){
+//     return "The computer's choice is correct!" ;
+//   }else{
+//     return "The computer's choice is wrong." + "The correct answer is:" + e ;
+//   }
+
+// }
+
+// console.log(getRandomQuestion(questions));
+// console.log(getRandomComputerChoice(questions));
+// console.log(getResults(questions));
+
+
+
+// function random(number){
+//   return Math.floor(Math.random()*number) ;
+// }
+
+
+
+
+
+
+
+
+
+
+// let normalizeUnits = (manifest) => {
+// let copy = {...manifest} 
+//   if(copy.unit=="lb"){
+//     copy.weight = copy.weight*0.45;
+//     copy.unit = "kg"
+//   }
+//   return copy ; 
+// }
+
+// let validateManifest = (manifest) => {
+// let copy = {...manifest}
+// if(copy.containerId>0 && Number.isInteger(copy.containerId) && copy.destination != null && copy.weight>0 && copy.unit != null && typeof copy.hazmat === boolean){
+//   return new Object() ;
+// }else if(copy.containerId==null && copy.destination != null && copy.weight == null && copy.unit != null && copy.hazmat == null){
+//   copy.containerId = "Missing";
+//   copy.destination = "Missing";
+//   copy.weight = "Missing";
+//   copy.unit = "Missing";
+//   copy.hazmat = "Missing";
+//   return copy ;
+// }else if(copy.containerId==null && copy.destination != null && copy.weight == null && copy.unit != null && typeof copy.hazmat == "boolean"){
+//   copy.containerId = "Invalid" ;
+//   return copy.containerId ;
+// }
+// else if(copy.containerId == null){
+//   return {containerId: "Invalid"} ;
+// }else if(copy.containerId == 0 && Number.isInteger(copy.destination) == true && copy.weight<0 && copy.unit != "kg" && copy.unit == "pounds" && typeof copy.hazmat != "boolean"){
+//   copy.containerId = "Invalid";
+//   copy.destination = "Invalid";
+//   copy.weight = "Invalid";
+//   copy.unit = "Invalid";
+//   copy.hazmat = "Invalid"; 
+//   return copy ;
+// }else if(copy.containerId < 0){
+//   copy.containerId = "Invalid";
+//   copy.destination = "Missing";
+//   copy.weight = "Missing";
+//   copy.unit = "Missing";
+//   copy.hazmat = "Missing"; 
+//   return copy ;
+// }else if(number.isInteger(copy.conatainerId) = false){
+//   copy.containerId = "Invalid"
+//   copy.destination = "Missing";
+//   copy.weight = "Missing";
+//   copy.unit = "Missing";
+//   copy.hazmat = "Missing"; 
+//   return copy ;
+// }else if(copy.destination.trim() = null){
+//   copy.containerId = "Missing"
+//   copy.destination = "Invalid";
+//   copy.weight = "Missing";
+//   copy.unit = "Missing";
+//   copy.hazmat = "Missing"; 
+//   return copy ;
+// }else if(Number.isNaN(copy.weight) == true){
+//   copy.containerId = "Missing";
+//   copy.destination = "Missing" ;
+//   copy.weight = "Invalid";
+//   copy.unit = "Missing";
+//   copy.hazmat = "Missing"; 
+//   return copy ;
+// }
+// }
+
+
+// let processManifest = (manifest) => {
+//   if(Number.isInteger(manifest.containerId) == true && manifest.containerId>0 && isNaN(manifest.destination)== true && manifest.destination!= null && Number.isInteger(manifest.weight) == true && manifest.weight>0 && manifest.unit != "kg" && manifest.unit != "lb" && typeof manifest.hazmat == boolean){
+//   console.log(`Validation success: ${manifest.containerId}`);
+//   normalizeUnits(manifest);
+//   console.log(`Total weight:${manifest.weight}`);
+// }else if(manifest.containerId<0 && Number.isNaN(manifest.weight)==true && Number.isInteger(manifest.destination)==false && manifest.destination != null){
+//     console.log(`Validation error:${manifest.containerId}`)
+//     manifest.containerId = "Invalid";
+//     manifest.weight = "Invalid";
+//     manifest.unit = "Missing";
+//     manifest.hazmat = "Missing";
+//     console.log(manifest);
+// }else if(manifest.containerId == null && manifest.weight == null && Number.isInteger(manifest.destination)==false && typeof manifest.hazmat == boolean){
+//   console.log("Validation error: undefined");
+//   manifest.containerId = "Missing";
+//   manifest.weight = "Missing";
+//   manifest.unit = "Missing";
+//   console.log(manifest);
+// }else if(manifest.containerId <= 0  && Number.isInteger(manifest.destination) == true && manifest.weight<0 && manifest.unit != "kg" &&manifest.unit == "lb" && typeof manifest.hazmat != boolean){
+//  console.log(`Validation error: ${manifest.containerId}`
+//  );
+//  console.log(validateManifest(manifest))
+// }
+// }
+
+const config1 = {
+  fault: false,
+  phases: [
+    { color: "green", duration: 5 },
+    { color: "yellow", duration: 2 },
+    { color: "red", duration: 4 }
+  ]
+};
+
+const config2 = {
+  fault: false,
+  phases: [
+    { color: "red", duration: 3 },
+    { color: "yellow", duration: -2 },
+    { color: "green", duration: 6 }
+  ]
+};
+
+const config3 = {
+  fault: true,
+  phases: [
+    { color: "green", duration: 5 },
+    { color: "yellow", duration: 2 },
+    { color: "red", duration: 6 }
+  ]
+};
+
+const config4 = {
+  fault: false,
+  phases: []
+};
+
+
+function runSequence (config,cycles) {
+for(let i=1; i<=cycles; i++) {
+   if(config.phases.length == 0){
+      return "No phases found";
+    }
+  for(let val in config.phases){
+   if(config.fault===true){
+      return "Faulted phase!";
+    }else if(config.phases[val].duration<=0){
+      return "Invalid phase detected"
+    }else{
+      console.log(`Switching to ${config.phases[val].color} for ${config.phases[val].duration} s`)
+    }
   }
-
-}
-
-console.log(getRandomQuestion(questions));
-console.log(getRandomComputerChoice(questions));
-console.log(getResults(questions));
-
-
-
-function random(number){
-  return Math.floor(Math.random()*number) ;
-}
-
-
-
-
-
-
-
-
-
-
-let normalizeUnits = (manifest) => {
-let copy = {...manifest} 
-  if(copy.unit=="lb"){
-    copy.weight = copy.weight*0.45;
-    copy.unit = "kg"
-  }
-  return copy ; 
-}
-
-let validateManifest = (manifest) => {
-let copy = {...manifest}
-if(copy.containerId>0 && Number.isInteger(copy.containerId) && copy.destination != null && copy.weight>0 && copy.unit != null && typeof copy.hazmat === boolean){
-  return new Object() ;
-}else if(copy.containerId==null && copy.destination != null && copy.weight == null && copy.unit != null && copy.hazmat == null){
-  copy.containerId = "Missing";
-  copy.destination = "Missing";
-  copy.weight = "Missing";
-  copy.unit = "Missing";
-  copy.hazmat = "Missing";
-  return copy ;
-}else if(copy.containerId==null && copy.destination != null && copy.weight == null && copy.unit != null && typeof copy.hazmat == "boolean"){
-  copy.containerId = "Invalid" ;
-  return copy.containerId ;
-}
-else if(copy.containerId == null){
-  return {containerId: "Invalid"} ;
-}else if(copy.containerId == 0 && Number.isInteger(copy.destination) == true && copy.weight<0 && copy.unit != "kg" && copy.unit == "pounds" && typeof copy.hazmat != "boolean"){
-  copy.containerId = "Invalid";
-  copy.destination = "Invalid";
-  copy.weight = "Invalid";
-  copy.unit = "Invalid";
-  copy.hazmat = "Invalid"; 
-  return copy ;
-}else if(copy.containerId < 0){
-  copy.containerId = "Invalid";
-  copy.destination = "Missing";
-  copy.weight = "Missing";
-  copy.unit = "Missing";
-  copy.hazmat = "Missing"; 
-  return copy ;
-}else if(number.isInteger(copy.conatainerId) = false){
-  copy.containerId = "Invalid"
-  copy.destination = "Missing";
-  copy.weight = "Missing";
-  copy.unit = "Missing";
-  copy.hazmat = "Missing"; 
-  return copy ;
-}else if(copy.destination.trim() = null){
-  copy.containerId = "Missing"
-  copy.destination = "Invalid";
-  copy.weight = "Missing";
-  copy.unit = "Missing";
-  copy.hazmat = "Missing"; 
-  return copy ;
-}else if(Number.isNaN(copy.weight) == true){
-  copy.containerId = "Missing";
-  copy.destination = "Missing" ;
-  copy.weight = "Invalid";
-  copy.unit = "Missing";
-  copy.hazmat = "Missing"; 
-  return copy ;
 }
 }
 
 
-let processManifest = (manifest) => {
-  if(Number.isInteger(manifest.containerId) == true && manifest.containerId>0 && isNaN(manifest.destination)== true && manifest.destination!= null && Number.isInteger(manifest.weight) == true && manifest.weight>0 && manifest.unit != "kg" && manifest.unit != "lb" && typeof manifest.hazmat == boolean){
-  console.log(`Validation success: ${manifest.containerId}`);
-  normalizeUnits(manifest);
-  console.log(`Total weight:${manifest.weight}`);
-}else if(manifest.containerId<0 && Number.isNaN(manifest.weight)==true && Number.isInteger(manifest.destination)==false && manifest.destination != null){
-    console.log(`Validation error:${manifest.containerId}`)
-    manifest.containerId = "Invalid";
-    manifest.weight = "Invalid";
-    manifest.unit = "Missing";
-    manifest.hazmat = "Missing";
-    console.log(manifest);
-}else if(manifest.containerId == null && manifest.weight == null && Number.isInteger(manifest.destination)==false && typeof manifest.hazmat == boolean){
-  console.log("Validation error: undefined");
-  manifest.containerId = "Missing";
-  manifest.weight = "Missing";
-  manifest.unit = "Missing";
-  console.log(manifest);
-}else if(manifest.containerId <= 0  && Number.isInteger(manifest.destination) == true && manifest.weight<0 && manifest.unit != "kg" &&manifest.unit == "lb" && typeof manifest.hazmat != boolean){
- console.log(`Validation error: ${manifest.containerId}`
- );
- console.log(validateManifest(manifest))
-}
+// function runSequence (config,cycles) {
+//   for(let i=0; i<=cycles; i++){
+//     if(config.phases==null){
+//       return "No phases found";
+//     }else if(config.fault===true){
+//       return "Faulted phase!";
+//     }else if(config.phases[i].duration<=0){
+//       return "Incalid phase detected"
+//     }else{
+//       console.log(`Switching to ${config.phases[i].color} for ${config.phases[i].durartion} s`)
+//     }
+//   }
+// }
+
+
+console.log(runSequence(config2,1));
+
+
+
+
+let arr = [5,2,4];
+let arr1 = [];
+
+for(let i=0; i<arr.length; i++){
+  arr1.push(arr[i]+arr[i+1]);
+  console.log(arr1);
 }
